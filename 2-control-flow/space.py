5eef8cc32525a382be36bb16357cc0e92a071ed9
Matt Licata <mattjlicata@gmail.com>
2021-02-24 10:03:50 -0500

    update space.py

--- 2-control-flow/space.py
+++ 2-control-flow/space.py
@@ -1,5 +1,5 @@
-# Space Boxer 🥊
-# Sonny Li
+# Space Boxer
+# Matt Licata (mattRicotta)
 
 print("I have information for the following planets:\n")
 
@@ -8,20 +8,21 @@ print("   4. Saturn  5. Uranus  6. Neptune\n")
  
 weight = 185
 planet = 3
+rel_gravity = 0
 
 # Write an if statement below:
-
 if planet == 1:
-  weight = weight * 0.78
+    rel_gravity = 0.91
 elif planet == 2:
-  weight = weight * 0.39
+    rel_gravity = 0.38
 elif planet == 3:
-  weight = weight * 2.65
+    rel_gravity = 2.34
 elif planet == 4:
-  weight = weight * 1.17
+    rel_gravity = 1.06
 elif planet == 5:
-  weight = weight * 1.05
+    rel_gravity = 0.92
 elif planet == 6:
-  weight = weight * 1.23
+    rel_gravity = 1.19
 
-print("Your weight:", weight)
+weight = weight * rel_gravity
+print("Your weight: ", weight)
