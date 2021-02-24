35d9e0cec827aaa0681b5f7cb952d24e8c8325ee
Matt Licata <mattjlicata@gmail.com>
2021-02-24 10:00:00 -0500

    update initials.py

--- 1-hello-world/block-letters/initials.py
+++ 1-hello-world/block-letters/initials.py
@@ -1,10 +1,11 @@
-# Sonny Li
-# Fun Fact: I played guitar in a band called Attica.
+# Matt Licata
+# Fun Fact: When I was 12 I went to space camp
 
-print("  SSS   L     ")
-print(" S   S  L     ")
-print(" S      L     ")
-print("  SSS   L     ")
-print("     S  L     ")
-print(" S   S  L     ")
-print("  SSS   LLLLL ")
+initials = """
+M   M L
+MM MM L
+M M M L
+M   M LLLLL
+"""
+
+print(initials)
